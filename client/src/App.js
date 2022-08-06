153a5cc917574484b0b3230ece503445e99f34bf
Shivam Pandey <sap0442@gmail.com>
2022-08-06 08:12:54 +0000

    added google calendar auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { GoogleLogin } from "react-google-login"
 
 function App() {
+
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
+
+  const responseError = (err) => {
+    console.log(err)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <h1>Google Calendar API</h1>
+      </div>
+      <div>
+        <GoogleLogin
+          clientId='585457191776-7p0k0lj23jg5bole92j44tvp2jfldcbc.apps.googleusercontent.com'
+          buttonText='Authenticate Your Google Calendar'
+          onSuccess={responseGoogle}
+          onFailure={responseError}
+          cookiePolicy={"single_host_origin"}
+          // This is important
+          responseType="code"
+          accessType='offline'
+          scope='openid email profile https://www.googleapis.com/auth/calendar '
+        />
+
+      </div>
     </div>
   );
 }
