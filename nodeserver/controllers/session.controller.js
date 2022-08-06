23c174849673f51c2b744518df8a01f1a135fd73
Shivam Pandey <sap0442@gmail.com>
2022-08-06 13:01:45 +0000

    google middleware and session creating

--- nodeserver/controllers/session.controller.js
+++ nodeserver/controllers/session.controller.js
@@ -2,6 +2,7 @@ import express from "express";
 import Sessions from "../models/session.model.js";
 import Users from "../models/user.model.js";
 import { authenicateDev, authenicateLogin } from "../middlewares/authenticate.js";
+import googleCalendar from "../middlewares/googleCalendar.js";
 
 const sessionRouter = express.Router();
 
@@ -13,13 +14,17 @@ sessionRouter.get("", authenicateLogin, async (req, res) => {
             const session = await Users.findOne({ "username": username }).populate({ path: "sessions", populate: ["session"] }).lean().exec();
             return res.status(200).json(session);
         }
+
+        // TODO: Add calendar over start time 
+        // TOLEARN : Date object.
         return res.send(`<form action="" method="post" >
         <input type="text" name="summary" placeholder="Enter the Summary of Session" />
         <input type="text" name="location" placeholder="Enter the location of Session" />
-        <textarea  name="description" placeholder="Description" width=300 height=500 >${req.session.sessionDescription?req.session.sessionDescription:""}</textarea>
+        <textarea  name="description" placeholder="Description" width=300 height=500 >${req.session.sessionDescription ? req.session.sessionDescription : ""}</textarea>
         <input type="checkbox" name="save"/>
         <input type="number" name="day" placeholder="Start Day" />
-        <input type="number" name="sessionStart" placeholder="Start Time" />
+        <input type="text" name="sessionStart" placeholder="Start Time" />
+        <input type="text" name="sessionEnd" placeholder="End Time" /> 
         <input type="number" name="slots" placeholder="Number of slots to open" />
         <input type="number" name="duration" placeholder="Duration of each slots" />
         <input type="number" name="colorId" placeholder="Color Code from 1 to 9" />
@@ -30,16 +35,30 @@ sessionRouter.get("", authenicateLogin, async (req, res) => {
     }
 })
 
-sessionRouter.post("", authenicateLogin, async (req, res) => {
+// in port route there is some bugs
+sessionRouter.post("", authenicateLogin, googleCalendar, async (req, res) => {
     try {
-        const {summary,description,location,sessionStart,duration,slots,save} = req.body;
-        if(save == "on") {
+        const { summary, description, location, sessionEnd, sessionStart, duration, slots, save } = req.body;
+        if (save == "on") {
             req.session.sessionDescription = req.body.description;
         }
+        const session = await Sessions.create(JSON.stringify({
+            sessionStart: sessionStart,
+            sessionEnd: sessionEnd,
+            slots: slots,
+            summary: summary,
+            description: description,
+            location: location,
+            timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone
+        }));
+        // let user = await Users.findOne({username:req.session.username}).lean().exec();
+        // if(!user) return res.redirect("/login");
+        console.log(session)
+        // await Users.findOneAndUpdate({username:req.session.username},{sessions:[...user.sessions,session._id]}).lean().exec()
 
         return res.send(`<h1>session created</h1>`)
     } catch (error) {
-        return res.status(500).json({message:"Internal server error"})        
+        return res.status(500).json({ message: "Internal server error" })
     }
 })
 
--- nodeserver/middlewares/googleCalendar.js
+++ nodeserver/middlewares/googleCalendar.js
@@ -10,32 +10,31 @@ const oAuth2Client = new OAuth2(
   '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'
 )
 
-// Call the setCredentials method on our oAuth2Client instance and set our refresh token.
-oAuth2Client.setCredentials({
-  refresh_token: '1//0441Vww7JFRzsCgYIARAAGAQSNwF-L9IrJcy--ymSrHu59pSePOVoU_r-B9EnkM_okCrF29OHKeDHfHCrh784q2PHFaszuM6n2bw',
-})
+
 
 
 const googleCalendar = (req, res, next) => {
+  // Call the setCredentials method on our oAuth2Client instance and set our refresh token.
+  oAuth2Client.setCredentials({
+    refresh_token: req.body.refresh_token || '1//0441Vww7JFRzsCgYIARAAGAQSNwF-L9IrJcy--ymSrHu59pSePOVoU_r-B9EnkM_okCrF29OHKeDHfHCrh784q2PHFaszuM6n2bw',
+  })
+
+  const { summary, description, location, sessionStart,sessionEnd, duration, slots, save, day, colorId } = req.body;
   // Create a new calender instance.
-  
+
   const calendar = google.calendar({ version: 'v3', auth: oAuth2Client })
 
   // Create a new event start date instance for temp uses in our calendar.
-  const eventStartTime = new Date()
-  eventStartTime.setDate(eventStartTime.getDay() + 2)
-
-  // Create a new event end date instance for temp uses in our calendar.
-  const eventEndTime = new Date()
-  eventEndTime.setDate(eventEndTime.getDay() + 4)
-  eventEndTime.setMinutes(eventEndTime.getMinutes() + 45)
+  const eventStartTime = new Date(sessionStart)
+  const eventEndTime = new Date(sessionEnd)
+  // console.log(day)
 
   // Create a dummy event for temp uses in our calendar
   const event = {
-    summary: `Meeting with Shiv`,
-    location: `Where noone can go but everyone can go`,
-    description: `Meet with shiv to talk about the new client project and how to integrate the calendar for booking.`,
-    colorId: 1,
+    summary: summary,
+    location: location,
+    description: description,
+    colorId: colorId,
     start: {
       dateTime: eventStartTime,
       timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
@@ -45,42 +44,50 @@ const googleCalendar = (req, res, next) => {
       timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
     },
   }
-
+  console.log(eventStartTime,eventEndTime);
   // Check if we a busy and have an event on our calendar for the same time.
-  calendar.freebusy.query(
-    {
-      resource: {
-        timeMin: eventStartTime,
-        timeMax: eventEndTime,
-        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-        items: [{ id: 'primary' }],
-      },
-    },
-    (err, res) => {
-      // Check for errors in our query and log them if they exist.
-      if (err) return console.error('Free Busy Query Error: ', err)
+  // calendar.freebusy.query(
+  //   {
+  //     resource: {
+  //       timeMin: eventStartTime,
+  //       timeMax: eventEndTime,
+  //       timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+  //       items: [{ id: 'primary' }],
+  //     },
+  //   },
+  //   (err, res) => {
+  //     // Check for errors in our query and log them if they exist.
+  //     if (err) return console.error('Free Busy Query Error: ', err)
 
-      // Create an array of all events on our calendar during that time.
-      const eventArr = res.data.calendars.primary.busy
+  //     // Create an array of all events on our calendar during that time.
+  //     const eventArr = res.data.calendars.primary.busy
 
-      // Check if event array is empty which means we are not busy
-      if (eventArr.length === 0)
-        // If we are not busy create a new calendar event.
-        return calendar.events.insert(
-          { calendarId: 'primary', resource: event },
-          err => {
-            // Check for errors and log them if they exist.
-            if (err) return console.error('Error Creating Calender Event:', err)
-            // Else log that the event was created.
-            return console.log('Calendar event successfully created.')
-          }
-        )
+  //     // Check if event array is empty which means we are not busy
+  //     if (eventArr.length === 0)
+  //       // If we are not busy create a new calendar event.
+  //       return calendar.events.insert(
+  //         { calendarId: 'primary', resource: event },
+  //         err => {
+  //           // Check for errors and log them if they exist.
+  //           if (err) return console.error('Error Creating Calender Event:', err)
+  //           // Else log that the event was created.
+  //           return console.log('Calendar event successfully created.')
+  //         }
+  //       )
 
-      // If event array is not empty log that we are busy.
-      return console.log(`Sorry I'm busy...`)
+  //     // If event array is not empty log that we are busy.
+  //     return console.log(`Sorry I'm busy...`)
+  //   }
+  // )
+  calendar.events.insert(
+    { calendarId: 'primary', resource: event },
+    err => {
+      // Check for errors and log them if they exist.
+      if (err) return res.redirect("/error")
+      // Else log that the event was created.
+      return next()
     }
   )
-
 }
 
 export default googleCalendar;
